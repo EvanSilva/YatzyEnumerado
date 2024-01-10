e393de32e386488b548c5ca3c59bb30a2159061e
Yelko Veiga <yelkovq@gmail.com>
2024-01-10 21:16:44 +0100

    refactor(yatzy.py): refactoring __init__

--- yatzy.py
+++ yatzy.py
@@ -39,14 +39,8 @@ class Yatzy:
         THREE = 3
         return dices.count(THREE) * THREE
     
-
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
+    def __init__(self, *dices):
+        self.dice = list(dices)
     
     def fours(self):
         sum = 0
