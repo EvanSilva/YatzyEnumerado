66ee9d169a418832dd453149cfb4614e4de7138c
Yelko Veiga <yelkovq@gmail.com>
2024-01-11 00:15:33 +0100

    refactor(yatzy.py): refactoring largestraight

--- yatzy.py
+++ yatzy.py
@@ -117,20 +117,11 @@ class Yatzy:
     
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
-            return 20
-        return 0
+    def largeStraight(*dices):
+        TWENTY = 20
+
+        return TWENTY if sorted(dices) == [2,3,4,5,6] else Yatzy.ZERO
+        
     
 
     @staticmethod
